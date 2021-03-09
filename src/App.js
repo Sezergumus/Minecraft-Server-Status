fa3775c1c97e3d72eafb10b78bde57a5bdd802f4
Sezer <sezer2004@gmail.com>
2021-03-09 22:06:14 +0300

    v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    // You can change after the status?ip= part to use it for a different server
+    fetch('https://mcapi.us/server/status?ip=mc.interaktifis.fun')
+      .then(response => response.json())
+        .then(Data => setData(Data));
+  }, [])
+
+  if (!data) {
+    return <span>Loading...</span>;
+  }
+  if (data.error) {
+    return <span>{data.error}</span>;
+  }
+
+  return(
+    <div>
+      <div className="background-container"></div>
+      <div className="app-container">
+        <img src="https://minecraftskinstealer.com/achievement/2/Interaktifis+Minecraft+/mc.interaktifis.fun" className="minecraft-achievement" alt="Minecraft Achievement" /> 
+        <table>
+          <tbody>
+            <tr className="top" id="first">
+              <td>
+                <p>Sunucu Durumu</p>            
+              </td>
+              <td>
+                {data.online ? <span style={{color:"green"}}>Online</span> : <span style={{color:"red"}}>Offline</span>}
+              </td>
+            </tr>
+            <tr className="top" id="second">
+              <td>
+                <p>Oyuncular</p>       
+              </td>
+              <td>
+                <span>({data.players.now}/{data.players.max})</span>       
+              </td>
+            </tr>
+
+          <tr>     
+            <ul>
+              {data.players.now > 0 &&
+                data.players.sample.map((player) => (
+                  <li className="tooltip">
+                    <img className="avatar tooltip" height={32} width={32} src={`https://minotar.net/avatar/${player.name}`}/>
+                    <span class="tooltiptext">{player.name}</span>
+                  </li>
+              ))}
+            </ul>
+          </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
